05a6443d095a51ee1597724c8daeaa98d4a6c6de
Eduardo Pucinelli <dudupucinelli@gmail.com>
2023-02-12 19:55:19 -0300

    adding files

--- src/main/java/br/com/beachtennisbrazil/api/system/configuration/WebSecurityConfig.java
+++ src/main/java/br/com/beachtennisbrazil/api/system/configuration/WebSecurityConfig.java
@@ -1,58 +1,25 @@
 package br.com.beachtennisbrazil.api.system.configuration;
 
-import br.com.beachtennisbrazil.api.system.service.LoginInterface;
-import br.com.beachtennisbrazil.api.system.serviceimpl.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter */ {
-
-/*
-    @Autowired
-    private LoginService service;
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+@Configuration
+@EnableWebMvc
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                        "/static/img/**",
+                        "/css/**",
+                        "/libs/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/libs/");
     }
-
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(service);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/api/**")
-                .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/dashboard/login")
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout")
-                .permitAll();
-    }
-
- */
-
 }
 
 
--- src/main/java/br/com/beachtennisbrazil/api/system/thymeleaf/LoginWebController.java
+++ src/main/java/br/com/beachtennisbrazil/api/system/thymeleaf/LoginWebController.java
@@ -1,11 +1,15 @@
 package br.com.beachtennisbrazil.api.system.thymeleaf;
 
+import br.com.beachtennisbrazil.api.system.dto.LoginDTO;
 import br.com.beachtennisbrazil.api.system.entities.Login;
+import br.com.beachtennisbrazil.api.system.entities.Register;
 import br.com.beachtennisbrazil.api.system.repositories.LoginRepository;
 import br.com.beachtennisbrazil.api.system.serviceimpl.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,9 +23,18 @@ public class LoginWebController {
     @Autowired
     private LoginService service;
 
-    @GetMapping (value = "dashboard/login")
-    public String login() {
-        return "login";
+    @GetMapping("system/login")
+    public ModelAndView tryConnect() {
+        ModelAndView mav = new ModelAndView("login");
+        Login login = new Login();
+        mav.addObject("login", login);
+        return mav;
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticate(@ModelAttribute Login login) {
+        service.authenticate(login);
+        return "redirect:/home";
     }
 
     @GetMapping(value = "/list-logins")
